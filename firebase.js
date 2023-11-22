2b712e3d5760b5a892e93586f86c9de239561756
AnhChienVu <vuanhchien003@gmail.com>
2023-11-22 00:10:30 -0500

    push file

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCQze76HRWOZ2FhyncY4s7I9VwNaOl8uRg",
+  authDomain: "tiktokphoneclonesnap.firebaseapp.com",
+  projectId: "tiktokphoneclonesnap",
+  storageBucket: "tiktokphoneclonesnap.appspot.com",
+  messagingSenderId: "905937468926",
+  appId: "1:905937468926:web:d252d58a64d8bed70c14af",
+  measurementId: "G-Y4SN55YHXV",
+};
